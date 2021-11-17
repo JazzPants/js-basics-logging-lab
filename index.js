e99df83847e3587c0399544ad439ce9e111e7bc9
JazzPants <leighton.l14@gmail.com>
2021-11-17 21:18:21 +1100

    Second Commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log('this code was called');
 
 console.log(driver);
 
-
-console.log(`${driver}`, 'is the driver variable name'); //test solution with two arguments
+console.log(driver, 'is the driver variable name');
+//console.log(`${driver}`, 'is the driver variable name'); //test solution with two arguments
 //console.log(`${driver} is the driver variable name`); also works
 //both -> bob is the driver variable name
